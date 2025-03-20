d7a37ee0a43f87a71676d1202190aed0dbb28b79
SaidAsaad <saidyousef201513@gmail.com>
2025-03-20 16:24:01 -0300

    Project created

---
+++ src/test/java/com/said/apps/CourseProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.said.apps;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
