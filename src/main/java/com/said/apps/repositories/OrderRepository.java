b91e84f7a3e592926271d91450f5cd5e2c4583b6
SaidAsaad <saidyousef201513@gmail.com>
2025-04-16 14:06:57 -0300

    Order, Instant, ISO 8601

---
+++ src/main/java/com/said/apps/repositories/OrderRepository.java
@@ -0,0 +1,10 @@
+package com.said.apps.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.said.apps.entities.Order;
+
+public interface OrderRepository extends JpaRepository<Order, Long> {
+
+	
+}
