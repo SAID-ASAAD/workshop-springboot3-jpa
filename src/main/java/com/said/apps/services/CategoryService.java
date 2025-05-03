d70b6df844161d5bfdfa85fa2a320e59ecbab7c3
SaidAsaad <saidyousef201513@gmail.com>
2025-05-03 14:02:54 -0300

    Categories delete

--- src/main/java/com/said/apps/services/CategoryService.java
+++ src/main/java/com/said/apps/services/CategoryService.java
@@ -4,10 +4,13 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import com.said.apps.entities.Category;
 import com.said.apps.repositories.CategoryRepository;
+import com.said.apps.services.exceptions.DatabaseException;
+import com.said.apps.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class CategoryService {
@@ -25,5 +28,17 @@ public class CategoryService {
 	
 	public Category insert(Category obj) {
 		return repository.save(obj);
+	}
+	
+	public void delete(Long id) {
+		try {
+			if(!repository.existsById(id)) {
+				throw new ResourceNotFoundException(id);
+			}
+			repository.deleteById(id);
+		}
+		catch (DataIntegrityViolationException e) {
+			throw new DatabaseException(e.getMessage());
+		}
 	}
 }
