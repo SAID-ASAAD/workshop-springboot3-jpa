1ccb0fe150411daed7264d76e43514c07719ccbc
SaidAsaad <saidyousef201513@gmail.com>
2025-05-03 13:32:44 -0300

    Update product

--- src/main/java/com/said/apps/services/ProductService.java
+++ src/main/java/com/said/apps/services/ProductService.java
@@ -12,15 +12,18 @@ import com.said.apps.repositories.ProductRepository;
 import com.said.apps.services.exceptions.DatabaseException;
 import com.said.apps.services.exceptions.ResourceNotFoundException;
 
+import jakarta.persistence.EntityNotFoundException;
+
 @Service
 public class ProductService {
-	
+
 	@Autowired
 	ProductRepository repository;
-	
+
 	public List<Product> findAll() {
 		return repository.findAll();
 	}
+
 	public Product findById(Long id) {
 		Optional<Product> obj = repository.findById(id);
 		return obj.get();
@@ -29,16 +32,34 @@ public class ProductService {
 	public Product insert(Product obj) {
 		return repository.save(obj);
 	}
-	
+
 	public void delete(Long id) {
 		try {
 			if (!repository.existsById(id)) {
 				throw new ResourceNotFoundException(id);
 			}
 			repository.deleteById(id);
-		}
-		catch (DataIntegrityViolationException e) {
+		} catch (DataIntegrityViolationException e) {
 			throw new DatabaseException(e.getMessage());
 		}
 	}
+
+	public Product update(Long id, Product obj) {
+		try {
+			Product entity = repository.getReferenceById(id);
+			updateData(entity, obj);
+			return repository.save(entity);
+		} 
+		catch (EntityNotFoundException e) {
+			throw new ResourceNotFoundException(id);
+		}
+	}
+
+	public void updateData(Product entity, Product obj) {
+		entity.setName(obj.getName());
+		entity.setDescription(obj.getDescription());
+		entity.setPrice(obj.getPrice());
+		entity.setImgUrl(obj.getImgUrl());
+	}
+
 }
