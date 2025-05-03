fc5fb5d1ef02b8c3e5f7945ce131cfe01b7de2c9
SaidAsaad <saidyousef201513@gmail.com>
2025-05-03 13:08:11 -0300

    Product Delete

--- src/main/java/com/said/apps/services/ProductService.java
+++ src/main/java/com/said/apps/services/ProductService.java
@@ -4,10 +4,13 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import com.said.apps.entities.Product;
 import com.said.apps.repositories.ProductRepository;
+import com.said.apps.services.exceptions.DatabaseException;
+import com.said.apps.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class ProductService {
@@ -25,5 +28,17 @@ public class ProductService {
 
 	public Product insert(Product obj) {
 		return repository.save(obj);
+	}
+	
+	public void delete(Long id) {
+		try {
+			if (!repository.existsById(id)) {
+				throw new ResourceNotFoundException(id);
+			}
+			repository.deleteById(id);
+		}
+		catch (DataIntegrityViolationException e) {
+			throw new DatabaseException(e.getMessage());
+		}
 	}
 }
